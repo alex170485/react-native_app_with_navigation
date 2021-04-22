07aa9b92284e31a088ddf4c49811a3d8a7938a12
alex170485 <alex.170485@icloud.com>
2021-04-22 13:18:18 +0300

    added adapted menu to android device

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -1,6 +1,7 @@
 import {createAppContainer} from 'react-navigation'
 import {createStackNavigator} from 'react-navigation-stack'
 import {createBottomTabNavigator} from 'react-navigation-tabs'
+import {createMaterialBottomTabNavigator} from 'react-navigation-material-bottom-tabs'
 import {MainScreen} from "../Screen/MainScreen";
 import {PostScreen} from "../Screen/PostScreen";
 import {THEME} from "../theme";
@@ -38,20 +39,32 @@ const BookedNavigator = createStackNavigator({
         }
     }
 )
-const BottomNavigator = createBottomTabNavigator({
-        Post: {
-            screen: PostNavigator,
+const bottomTabsConfig = {
+    Post: {
+        screen: PostNavigator,
             navigationOptions: {
+            tabBarLabel: 'Все',
                 tabBarIcon: info => (<Ionicons name={'ios-albums'} size={25} color={info.tintColor}/>)
-            }
-        },
-        Booked: {
-            screen: BookedNavigator,
-            navigationOptions: {
-                tabBarIcon: info => (<Ionicons name={'ios-star'} size={25} color={info.tintColor}/>)
-            }
         }
-    }, {
+    },
+    Booked: {
+        screen: BookedNavigator,
+            navigationOptions: {
+            tabBarLabel: 'Избранное',
+                tabBarIcon: info => (<Ionicons name={'ios-star'} size={25} color={info.tintColor}/>)
+        }
+    }
+}
+
+const BottomNavigator = Platform.OS === 'android'
+    ? createMaterialBottomTabNavigator(bottomTabsConfig, {
+        activeTintColor: '#fff',
+        shifting: true,
+        barStyle: {
+            backgroundColor: THEME.MAIN_COLOR
+        }
+    })
+    : createBottomTabNavigator(bottomTabsConfig, {
         tabBarOptions: {
             activeTintColor: THEME.MAIN_COLOR
         }
